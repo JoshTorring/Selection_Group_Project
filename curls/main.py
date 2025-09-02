0abe0ac851950d8d9314314de2fb6b65fa9b5a18
nathanielclay <nathanielclay@proton.me>
2025-09-02 14:58:01 +0100

    Create main.py
    Adding pseudocode main py file

---
+++ curls/main.py
@@ -0,0 +1,19 @@
+# Main loop
+
+flying = True
+
+while flying:
+  
+  # get Data from api
+
+
+  # calculate drone position
+
+
+  # calculate direction change needed
+
+
+  # decide if we need to drop the bomb
+
+  # send commands to drone
+    # Either change geading or drop
